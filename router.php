362662e2dc1c657066db4570c63a599c08770652
Inti Derose <intiderose2003gmail.com>
2024-10-02 15:56:58 -0300

    templates, css y router

---
+++ router.php
@@ -0,0 +1,63 @@
+<?php
+require_once 'libs/response.php';
+require_once 'app/middlewares/session.auth.middleware.php';
+require_once 'app/controllers/task.controller.php';
+require_once 'app/controllers/auth.controller.php';
+
+// base_url para redirecciones y base tag
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$res = new Response();
+
+$action = 'listar'; // accion por defecto si no se envia ninguna
+if (!empty( $_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+// tabla de ruteo
+
+// listar  -> TaskController->showTask();
+// nueva  -> TaskController->addTask();
+// eliminar/:ID  -> TaskController->deleteTask($id);
+// finalizar/:ID -> TaskController->finishTask($id);
+// ver/:ID -> TaskController->view($id); COMPLETAR
+
+// parsea la accion para separar accion real de parametros
+$params = explode('/', $action);
+
+switch ($params[0]) {
+    case 'listar':
+        sessionAuthMiddleware($res); // Verifica que el usuario esté logueado y setea $res->user o redirige a login
+        $controller = new TaskController($res);
+        $controller->showTasks();
+        break;
+    case 'nueva':
+        sessionAuthMiddleware($res);
+        $controller = new TaskController($res);
+        $controller->addTask();
+        break;
+    case 'eliminar':
+        sessionAuthMiddleware($res);
+        $controller = new TaskController($res);
+        $controller->deleteTask($params[1]);
+        break;
+    case 'finalizar':
+        sessionAuthMiddleware($res);
+        $controller = new TaskController($res);
+        $controller->finishTask($params[1]);
+        break;
+    case 'showLogin':
+        $controller = new AuthController();
+        $controller->showLogin();
+        break;
+    case 'login':
+        $controller = new AuthController();
+        $controller->login();
+        break;
+    case 'logout':
+        $controller = new AuthController();
+        $controller->logout();
+    default: 
+        echo "404 Page Not Found"; // deberiamos llamar a un controlador que maneje esto
+        break;
+}
